b49f58d345ffc18080d101bced2ce92b605cd5a6
Gleidson Takahashi <gleidsontakahashi@gmail.com>
2022-11-03 21:04:06 -0400

    java-basico

---
+++ trilha-java-basico/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+       //Apresentar os critérios para criação de métodos, parâmetros e visibilidade.
+
+       
+        
+    }
+}
